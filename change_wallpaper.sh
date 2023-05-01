51bbc6b06ad03f4de706ec9c451ebd39d7471b64
roganofff <matematic1337@gmail.com>
2023-05-01 20:10:29 +0300

    1st commit

---
+++ change_wallpaper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# TODO
+# Check if there is a file that contains a hashed password:
+#   If not, create it and set up a password.
+# If such a file exists:
+#   Read from keyboard a password:
+#       If passwords match:
+#           Change the desktop wallpapers to random cat pic
+#       If they do not match:
+#           Lookup for web-camera:
+#               If there is a web-cam take a picture of thief's face and set up it as a wallpaper.
+#               If no web-cam was found, do nothing
+
