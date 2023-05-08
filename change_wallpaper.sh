998b363d771b1adc679dc4f4dda97828eb51370a
roganofff <matematic1337@gmail.com>
2023-05-09 02:22:08 +0300

    Wallpaper

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # TODO
-# Check if there is a file that contains a hashed password:
-#   If not, create it and set up a password.
-# If such a file exists:
-#   Read from keyboard a password:
-#       If passwords match:
-#           Change the desktop wallpapers to random cat pic
+# Check if there is a file that contains a hashed password: +
+#   If not, create it and set up a password. +
+# If such a file exists: +
+#   Read from keyboard a password: +
+#       If passwords match: +
+#           Change the desktop wallpapers to random cat pic:
+#               Check if internet connection is available
+#               Ping server
+#           (To change wallpaper you need to know what theme scheme is!!)
 #       If they do not match:
 #           Lookup for web-camera:
 #               If there is a web-cam take a picture of thief's face and set up it as a wallpaper.
 #               If no web-cam was found, do nothing
-# ещё такая фишечка, что на картинке с котом будет логин компа
+# ещё такая фишечка, что на картинке с котом будет логин компа +
 
+URL=https://cataas.com/cat/says/$USER
+PIC=wallpaper.jpg
 pswd_filename=password
 tn=$'\t\n'
 
@@ -56,16 +61,39 @@ function authorization {
     read password < $pswd_filename
 
     while true
-        read -p "Enter password: " -s inp_password
+    do
+        read -p "Enter password:$tn" -s inp_password
         inp_hash=`echo $inp_password | md5sum | awk '{print $1}'`
         if [[ $inp_hash == $password ]]; then
             echo "You were signed in successfully."
+            return 1
         else
-            echo "Try again."
+            echo "Incorrect password. You're a thief!"
+            return 0
+        fi
+    done
+}
+
+function change_wallpaper {
+    DIR=`find /home -name $PIC | grep WallpaperChanger/$PIC`
+    scheme=`gsettings get org.gnome.desktop.interface color-scheme`
+    if [[ "$SCHEME" == "'prefer-dark'" ]]; then
+        gsettings set org.gnome.desktop.background picture-uri-dark "file://$DIR"
+    else
+        gsettings set org.gnome.desktop.background picture-uri "file://$DIR"
+    fi
 }
 
 if [[ ! -f $pswd_filename ]]; then
     registration
+    curl $URL -o $PIC
+    change_wallpaper $PIC
+    echo "Wallpaper has been changed."
 else
     authorization
+    if [[ $? -eq 1 ]]; then
+        echo 1
+    else
+        echo 0
+    fi
 fi
