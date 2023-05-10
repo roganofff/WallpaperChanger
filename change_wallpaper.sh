2029ee094085ab117cb66a431d1d29d92f9f59ad
roganofff <matematic1337@gmail.com>
2023-05-10 23:32:07 +0300

    Ver 1.1. Small changes & upgrades

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -22,7 +22,7 @@ function write_hash_md5 {
 
 function download_cat {
     echo "Downloading a picture of a cat:3"
-    curl $1 -o $PIC
+    curl -s $1 -o $PIC > /dev/null
     change_wallpaper $PIC   
     echo "Wallpaper has been changed."
 }
@@ -64,17 +64,17 @@ function authorization {
         inp_hash=`echo $inp_password | md5sum | awk '{print $1}'`
         if [[ $inp_hash == $password ]]; then
             echo "You were signed in successfully."
-            return 1
+            return 0
         else
             echo "Incorrect password. You're a thief!"
-            return 0
+            return 1
         fi
     done
 }
 
 # Меняет обои на полученную картинку
 function change_wallpaper {
-    DIR=`find /home -name $PIC | grep WallpaperChanger/$PIC`
+    DIR=`find /home -name $PIC 2> /dev/null | grep WallpaperChanger/$PIC`
     scheme=`gsettings get org.gnome.desktop.interface color-scheme`
     if [[ "$scheme" == "'prefer-dark'" ]]; then
         gsettings set org.gnome.desktop.background picture-uri-dark file://$DIR
@@ -90,16 +90,16 @@ if [[ ! -f $pswd_filename ]]; then
     make_good_note_in_log   
 else
     authorization
-    if [[ $? -eq 1 ]]; then
+    if [[ $? -eq 0 ]]; then
         download_cat $URL
         echo "Making notes in log file."
         make_good_note_in_log
     else
         echo "Searching for a webcam..."
-        webcams=`find /dev/ -name video* | wc -l` 
-#       На ноутбуках HP пишет, что их две(/dev/video0 и /dev/video1), но на самом деле вторая нужна для метаданных
-        echo "Cameras found: $webcams."
-        if [[ $webcams -gt 0 ]]; then
+        webcam=`v4l2-ctl --list-devices 2> /dev/null | awk '/usb/{ getline; print $1 }' | wc -l` 
+#       На ноутбуках HP пишет хоть и всего одна камера, но в файлах их две(/dev/video0 и /dev/video1), но на самом деле вторая нужна для метаданных
+        echo "Cameras found: $webcam."
+        if [[ $webcam -gt 0 ]]; then
             echo "Taking a webcam shot..."
             fswebcam -r 1280x720 --jpeg 100 -D 1 $PIC
             sleep 1
@@ -114,7 +114,7 @@ else
             echo "No cameras was found."
             download_cat $URL2
             echo "Making notes in log file."
-            make_good_note_in_log
+            make_bad_note_in_log
         fi
     fi
 fi
